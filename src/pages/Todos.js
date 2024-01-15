a6386b0b957b19abcb972fb6e0049b9a4efa363a
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2024-01-15 11:36:08 +0330

    completed

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -1,8 +1,8 @@
 import { useContext, useEffect, useState } from "react";
-// import CreateTodo from "../components/todos/Create";
-// import DeleteTodo from "../components/todos/Delete";
-// import FilterTodos from "../components/todos/Filter";
-// import UpdateTodo from "../components/todos/Update";
+import CreateTodo from "../components/todos/Create";
+import DeleteTodo from "../components/todos/Delete";
+import FilterTodos from "../components/todos/Filter";
+import UpdateTodo from "../components/todos/Update";
 import TodoContext from "../context/TodoContext";
 
 const Todos = () => {
@@ -18,23 +18,27 @@ const Todos = () => {
 
     return (
         <div className="container mt-5">
-            <div className="row g-3">
-       
-                {error && <div>{error}</div>}
-                {loading && <div className="col-md-12 text-center"><div className="spinner-border mt-5"></div></div>}
-                {todos && todos.map(todo => (
-                    <div className="col-md-4" key={todo.id}>
-                        <div className={"card " + (todo.completed && "bg-light")}>
-                            <div className="card-body d-flex justify-content-between align-items-center">
-                                <div>{todo.completed ? <del>{todo.title}</del> : <span>{todo.title}</span>}</div>
-                                <div className="d-flex align-items-center">
-                                </div>
+        <div className="row g-3">
+            <CreateTodo />
+            <hr />
+            <FilterTodos />
+            {error && <div>{error}</div>}
+            {loading && <div className="col-md-12 text-center"><div className="spinner-border mt-5"></div></div>}
+            {todos && todos.map(todo => (
+                <div className="col-md-4" key={todo.id}>
+                    <div className={"card " + (todo.completed && "bg-light")}>
+                        <div className="card-body d-flex justify-content-between align-items-center">
+                            <div>{todo.completed ? <del>{todo.title}</del> : <span>{todo.title}</span>}</div>
+                            <div className="d-flex align-items-center">
+                                <UpdateTodo todo={todo} />
+                                <DeleteTodo todoId={todo.id} />
                             </div>
                         </div>
                     </div>
-                ))}
-            </div>
+                </div>
+            ))}
         </div>
+    </div>
     )
 }
 
