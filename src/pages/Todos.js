5e69a167204ff933e2509faa5f50750f97c0bf8a
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2024-01-15 11:17:38 +0330

    get data from api and show them on todo page

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -1,10 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+// import CreateTodo from "../components/todos/Create";
+// import DeleteTodo from "../components/todos/Delete";
+// import FilterTodos from "../components/todos/Filter";
+// import UpdateTodo from "../components/todos/Update";
 import TodoContext from "../context/TodoContext";
 
-const Todos = ()=> {
-    const todoContext = useContext(TodoContext)
-        return (
-        <h1>Todos - {todoContext}</h1>
+const Todos = () => {
+    const { getTodos, todos, error } = useContext(TodoContext);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        (async () => {
+            await getTodos();
+            setLoading(false);
+        })()
+    }, [getTodos]);
+
+    return (
+        <div className="container mt-5">
+            <div className="row g-3">
+       
+                {error && <div>{error}</div>}
+                {loading && <div className="col-md-12 text-center"><div className="spinner-border mt-5"></div></div>}
+                {todos && todos.map(todo => (
+                    <div className="col-md-4" key={todo.id}>
+                        <div className={"card " + (todo.completed && "bg-light")}>
+                            <div className="card-body d-flex justify-content-between align-items-center">
+                                <div>{todo.completed ? <del>{todo.title}</del> : <span>{todo.title}</span>}</div>
+                                <div className="d-flex align-items-center">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
+
 export default Todos;
